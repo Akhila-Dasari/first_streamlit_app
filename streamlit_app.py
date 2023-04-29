22113894eae8418b71d9c49b7d10b434f5e571e3
Akhila-Dasari <131961583+Akhila-Dasari@users.noreply.github.com>
2023-04-29 20:43:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,9 @@ streamlit.dataframe(my_fruit_list)
 
  # fruityvice API response
   
- # streamlit.header("Fruityvice Fruit Advice!")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
-  import requests
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-  streamlit.text(fruityvice_response)
+
 
